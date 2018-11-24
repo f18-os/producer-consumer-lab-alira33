25a28b9dcecbbc6ad8adc68cfefd16837af833cd
abigail.lira <al.abigail.lira@gmail.com>
2018-11-24 05:34:12 +0000

    added sentinel value to terminate and display on consumer

--- ExtractFrames.py
+++ ExtractFrames.py
@@ -12,6 +12,7 @@ from DisplayFrames import *
 import sys
 
 queue = []
+queue_frames = []
 # globals
 outputDir    = 'frames'
 clipFileName = 'clip.mp4'
@@ -19,12 +20,15 @@ clipFileName = 'clip.mp4'
 
 queue = []
 lock = Lock()
+terminate = False
 MAX_NUM = 10 #requirement completed: is being halved to 10
 condition = Condition()
 
 class ProducerThread(Thread):
   def run(self):
       global queue
+      global queue_frames
+      global terminate
       count = 0
       i = 0
 
@@ -56,29 +60,49 @@ class ProducerThread(Thread):
         #pylint: disable-msg=R0913
         grayFrame = GrayscaleThread(Thread, image, i)
 
-        #pylint: disable-msg=R0913
-        display(Thread, grayFrame, i)
-
+        queue_frames.append(grayFrame)
         queue.append(count)
         count += 1
         i += 1
+
+        if(image is None):
+            terminate = True
+            condition.wait()
 
       cv2.destroyAllWindows()
 
 class ConsumerThread(Thread):
     def run(self):
         global queue
+        global queue_frames
+        global terminate
+        i = 0
+
         while True:
             condition.acquire()
             if not queue:
                 print ("Nothing in queue, consumer is waiting")
                 condition.wait()
                 print ("Producer added something to queue and notified the consumer")
+            while i < len(queue_frames):
+                #pylint: disable-msg=R0913
+                display(Thread, queue_frames[i], i)
+                i += 1
             queue.pop(0)
             print ("Consumed extracting frames")
             condition.notify()
             condition.release()
             time.sleep(random.random())
+
+            if(terminate):
+                cv2.destroyAllWindows()
+                try:
+                    sys.exit()  # this always raises SystemExit
+                except SystemExit:
+                    print("sys.exit() worked as expected")
+                except:
+                    # some other exception got raised
+                    print("Something went horribly wrong")
  
 ProducerThread().start()
 ConsumerThread().start()
